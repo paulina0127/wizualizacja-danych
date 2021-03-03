5aa216201394b3c8a2c4284e99004028dd918556
paulina0127 <paulina.hryciuk@student.uwm.edu.pl>
2021-03-03 20:19:04 +0100

    Dodanie lab1

---
+++ labs/lab1.py
@@ -0,0 +1,108 @@
+from math import *
+
+"""
+Zad1
+
+a = 5
+b = 7
+print(u"Liczby całkowite: ", a, b)
+
+c = 5.2
+d = 9.5
+print("Liczby rzeczywiste: ", c, d)
+
+napis1 = "tekst"
+napis2 = "imie"
+print("Napisy: ", napis1, napis2)
+
+e = 2 + 3j
+f = 4 + 0.5j
+print("Liczby zespolone: ", e, f)
+"""
+
+"""
+Zad2
+
+a = 25
+b = 5
+
+dodawanie = a + b
+odejmowanie = a - b
+mnozenie = a * b
+dzielenie = a / b
+dzielenie_calkowite = a // b
+reszta_z_dzielenia = a % b
+potega = a ** b
+pierwiastek = sqrt(a)
+
+print(dodawanie, odejmowanie, mnozenie, dzielenie, dzielenie_calkowite, reszta_z_dzielenia, potega, pierwiastek)
+"""
+
+"""
+Zad3
+
+a = 2
+
+a += 1
+a -= 1
+a *= 2
+a /= 2
+a **= 2
+a %= 2
+
+print(a)
+"""
+
+"""
+Zad4
+
+a = exp(10)
+b = (log(5+sin(8)**2))**(1/6.0)
+c = floor(3.55)
+d = ceil(4.80)
+
+print(a,b,c,d)
+"""
+
+"""
+Zad5
+
+imie = "PAULINA"
+nazwisko = "HRYCIUK"
+
+print(imie.capitalize(), nazwisko.capitalize())
+"""
+
+"""
+Zad6
+
+tekst = "la la land"
+
+print(tekst.count("la"))
+"""
+
+"""
+Zad7
+
+napis = "kotek"
+
+print(napis[1], napis[len(napis)-1])
+"""
+
+"""
+Zad8
+
+tekst = "la la land"
+
+print(tekst.split())
+"""
+
+"""
+Zad9
+
+a = 5.2
+b = "tekst"
+c = hex(54)
+
+print(u"Wartości zmiennych: %(z1)f, %(z2)s, %(z3)s" %{"z1":a, "z2":b, "z3":c})
+"""
